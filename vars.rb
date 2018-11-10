8052e8b33b28b0e9eba32b0038836b8b474e430a
zcashcloudz <webmasta@dashcloudz.online>
2018-11-10 01:11:07 +0100

    initial commit

---
+++ vars.rb
@@ -0,0 +1,29 @@
+@current_btc_price = "6500"
+@current_bch_price = "550"
+
+@coincode_short = "etc"
+@coincode_long = "Ethereum Classic"
+
+@algorithm = "Ethash"
+@denominator = "MH/s"
+
+@bronze_price="0.0090"
+@bronze_hash="300"
+
+@silver_price="0.0170"
+@silver_hash="600"
+
+@gold_price="0.0300"
+@gold_hash="1200"
+
+@platinum_price="0.0550"
+@platinum_hash="2400"
+
+@profit_single_hash="0.33"
+
+## dont change under this is not necessarily
+@site_name = "#{@coincode_short} mining"
+@site_title = "start #{@site_name} in our cloud infrastructure today!"
+@description = "#{@site_name} offers #{@coincode_short} cloud mining at competitive price. Start mining #{@coin_long} today!"
+@keywords = "#{@coincode_short} mining, #{@coincode_long} mining, cryptocurrency mining, #{@site_name}, gpu mining, asic mining, cpu mining, cloud mining"
+@author = "#{@site_name}"
